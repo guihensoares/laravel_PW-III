0cffe6727e8d12ff20b77a62fbfaabaa5fd11bac
guihensoares <guilhers53292@gmail.com>
2026-06-18 18:19:45 -0300

    date_alterado

--- resources/views/componente/atualizar.blade.php
+++ resources/views/componente/atualizar.blade.php
@@ -10,10 +10,10 @@
         <input type="text" name="nome" id="nome" value="{{ $componente->nome }}">
 
         <label for="hora_inicio">Hora do Inicio</label>
-        <input type="datetime" name="hora_inicio" id="hora_inicio" value="{{ $componente->hora_inicio }}">
+        <input type="date" name="hora_inicio" id="hora_inicio" value="{{ $componente->hora_inicio }}">
 
         <label for="hora_fim">Hora do Término</label>
-        <input type="datetime" name="hora_fim" id="hora_fim" value="{{ $componente->hora_fim }}">
+        <input type="date" name="hora_fim" id="hora_fim" value="{{ $componente->hora_fim }}">
 
         <button type="submit">Salvar</button>
         @isset($success)
--- resources/views/componente/index.blade.php
+++ resources/views/componente/index.blade.php
@@ -8,10 +8,10 @@
         <input type="text" name="nome" id="nome" value="{{ old('nome') }}">
 
         <label for="hora_inicio">Hora do Inicio</label>
-        <input type="datetime" name="hora_inicio" id="hora_inicio" value="{{ old('hora_inicio') }}">
+        <input type="date" name="hora_inicio" id="hora_inicio" value="{{ old('hora_inicio') }}">
 
         <label for="hora_fim">Hora do Término</label>
-        <input type="datetime" name="hora_fim" id="hora_fim" value="{{ old('hora_fim') }}">
+        <input type="date" name="hora_fim" id="hora_fim" value="{{ old('hora_fim') }}">
 
         <button type="submit">Salvar</button>
         @isset($success)
